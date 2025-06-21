ab48557a87971976d07ce970633d77add229742f
rjmahfuztech <rjmahfuztech@gmail.com>
2025-06-22 00:31:40 +0600

    Navbar and sidebar added

---
+++ src/components/NavSidebar.jsx
@@ -0,0 +1,81 @@
+import { FiMenu } from "react-icons/fi";
+import { TbXboxXFilled } from "react-icons/tb";
+import { Link } from "react-router";
+
+const NavSidebar = ({ drawerRef, closeDrawer, myLogo }) => {
+  return (
+    <div className="drawer drawer-end">
+      <input
+        id="my-drawer-4"
+        type="checkbox"
+        className="drawer-toggle"
+        ref={drawerRef}
+      />
+      <div className="drawer-content">
+        {/* Page content here */}
+        <label
+          htmlFor="my-drawer-4"
+          className="drawer-button p-6 bg-[#55E6A5] mr-1 cursor-pointer"
+        >
+          <FiMenu
+            strokeWidth={2}
+            className="size-6 text-black inline-block w-9 h-9"
+          />
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label
+          htmlFor="my-drawer-4"
+          aria-label="close sidebar"
+          className="drawer-overlay"
+        ></label>
+        <div className=" bg-[#09101A] text-white min-h-full w-[26rem] p-0">
+          <div className="flex justify-between items-center gap-2 bg-[#141C27] p-6">
+            <Link to="/">
+              <img className="w-32" src={myLogo} alt="Logo" />
+            </Link>
+            <TbXboxXFilled
+              onClick={closeDrawer}
+              className="w-11 h-11 text-[#55E6A5] transition-colors duration-500 cursor-pointer  hover:text-white rounded-full"
+            />
+          </div>
+          {/* Sidebar content here */}
+          <div className="px-7 mt-16 mb-8">
+            <h3 className="text-xl font-semibold">ABOUT ME</h3>
+            <p className="text-[#a1a3af] mt-6 text-lg">
+              Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod
+              tempor incididunt ut labore et magna aliqua. Ut enim ad minim
+              veniam laboris.
+            </p>
+            {/* Form */}
+            <h3 className="text-xl font-semibold mt-20">GET IN TOUCH</h3>
+            <form action="">
+              <input
+                type="text"
+                placeholder="Your Name"
+                className="input input-lg mt-4 px-4 py-8 bg-[#09101A] border-[#55E6A5] border-x-2 rounded-none w-full"
+              />
+              <input
+                type="text"
+                placeholder="Your Email"
+                className="input input-lg mt-4 px-4 py-8 bg-[#09101A] border-[#55E6A5] border-x-2 rounded-none w-full"
+              />
+              <textarea
+                placeholder="Message"
+                className="textarea textarea-lg mt-4 px-4 py-6 bg-[#09101A] border-[#55E6A5] border-x-2 rounded-none w-full"
+                rows={5}
+              ></textarea>
+              <input
+                type="submit"
+                value="Submit Now"
+                className="mt-4 px-6 py-4 cursor-pointer bg-[#55E6A5] text-black font-semibold text-lg hover:bg-[#141C27] hover:text-white transition-colors duration-500 border-none nav-animation"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavSidebar;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,120 +1,157 @@
 import { Link } from "react-router";
 import myLogo from "../assets/images/mahfuz-logo.jpeg";
-import { useRef } from "react";
+import { useRef, useState } from "react";
+import NavSidebar from "./NavSidebar";
 import { FiMenu } from "react-icons/fi";
-import { TbXboxXFilled } from "react-icons/tb";
+import { RxCross2 } from "react-icons/rx";
 
 const Navbar = () => {
   const drawerRef = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
 
   const closeDrawer = () => {
     if (drawerRef.current) {
       drawerRef.current.checked = false;
     }
   };
+
   return (
-    <div className="max-w-7xl mx-auto relative">
-      <div className="navbar justify-between p-0 bg-[#141C27] text-white shadow-sm absolute left-0 right-0">
-        <div className="">
-          <Link to="/">
-            <img className="w-56 p-1" src={myLogo} alt="Logo" />
-          </Link>
-        </div>
-        <div className="hidden lg:block uppercase nav-link">
-          <Link className="text-sm font-semibold mr-12 nav-animation" to="/">
-            Home
-          </Link>
-          <Link className="text-sm font-semibold mr-12 nav-animation" to="/">
-            About
-          </Link>
-          <Link className="text-sm font-semibold mr-12 nav-animation" to="/">
-            Service
-          </Link>
-          <Link className="text-sm font-semibold mr-12 nav-animation" to="/">
-            Project
-          </Link>
-          <Link className="text-sm font-semibold mr-12 nav-animation" to="/">
-            Blog
-          </Link>
-          <Link className="text-sm font-semibold nav-animation" to="/">
-            Contact
-          </Link>
-        </div>
-        <div className="">
-          <div className="drawer drawer-end">
-            <input
-              id="my-drawer-4"
-              type="checkbox"
-              className="drawer-toggle"
-              ref={drawerRef}
+    <>
+      <div className="max-w-[1400px] mx-auto relative">
+        <div className="navbar justify-between p-0 bg-[#141C27] text-white shadow-sm absolute left-0 right-0 px-4 lg:px-0">
+          <div>
+            <Link to="/">
+              <img
+                className="w-32 lg:w-56 p-1 md:p-1"
+                src={myLogo}
+                alt="Logo"
+              />
+            </Link>
+          </div>
+          <div className="hidden lg:block uppercase nav-link">
+            <Link
+              onClick={() => window.scrollTo(0, 0)}
+              className="text-sm font-semibold mr-12 nav-animation"
+              to="/"
+            >
+              Home
+            </Link>
+            <a
+              href="/#about"
+              className="text-sm font-semibold mr-12 nav-animation"
+            >
+              About
+            </a>
+            <a
+              href="/#service"
+              className="text-sm font-semibold mr-12 nav-animation"
+            >
+              Service
+            </a>
+            <a
+              href="/#project"
+              className="text-sm font-semibold mr-12 nav-animation"
+            >
+              Project
+            </a>
+            <a
+              href="/#skills"
+              className="text-sm font-semibold mr-12 nav-animation"
+            >
+              Skills
+            </a>
+            <Link
+              className="text-sm font-semibold mr-12 nav-animation"
+              to="/blog"
+            >
+              Blog
+            </Link>
+            <Link className="text-sm font-semibold nav-animation" to="/contact">
+              Contact
+            </Link>
+          </div>
+          {/* Nav sidebar */}
+          <div className="hidden lg:block">
+            <NavSidebar
+              drawerRef={drawerRef}
+              closeDrawer={closeDrawer}
+              myLogo={myLogo}
             />
-            <div className="drawer-content">
-              {/* Page content here */}
-              <label
-                htmlFor="my-drawer-4"
-                className="drawer-button p-6 bg-[#55E6A5] cursor-pointer"
-              >
+          </div>
+          <div className="lg:hidden">
+            <button
+              onClick={() => setIsOpen(!isOpen)}
+              className="bg-[#55E6A5] p-2"
+            >
+              <div className="relative w-5 h-5 flex justify-center items-center">
                 <FiMenu
-                  strokeWidth={2}
-                  className="size-6 text-black inline-block w-9 h-9"
+                  className={`absolute text-black transition-opacity duration-300 ${
+                    isOpen ? "opacity-0" : "opacity-100"
+                  }`}
                 />
-              </label>
-            </div>
-            <div className="drawer-side">
-              <label
-                htmlFor="my-drawer-4"
-                aria-label="close sidebar"
-                className="drawer-overlay"
-              ></label>
-              <div className=" bg-[#09101A] text-white min-h-full w-[26rem] p-0">
-                <div className="flex justify-between items-center gap-2 bg-[#141C27] p-6">
-                  <Link to="/">
-                    <img className="w-32" src={myLogo} alt="Logo" />
-                  </Link>
-                  <TbXboxXFilled
-                    onClick={closeDrawer}
-                    className=" w-11 h-11 text-[#55E6A5] transition-colors duration-500 cursor-pointer  hover:text-white rounded-full"
-                  />
-                </div>
-                {/* Sidebar content here */}
-                <div className="px-7 mt-16">
-                  <h3 className="text-xl font-semibold">ABOUT ME</h3>
-                  <p className="text-[#a1a3af] mt-6 text-lg">
-                    Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod
-                    tempor incididunt ut labore et magna aliqua. Ut enim ad
-                    minim veniam laboris.
-                  </p>
-                  {/* Form */}
-                  <h3 className="text-xl font-semibold mt-20">GET IN TOUCH</h3>
-                  <form action="">
-                    <input
-                      type="text"
-                      placeholder="Your Name"
-                      className="input input-lg mt-4 px-4 py-8 bg-[#09101A] border-[#55E6A5] border-x-2 rounded-none w-full"
-                    />
-                    <input
-                      type="text"
-                      placeholder="Your Email"
-                      className="input input-lg mt-4 px-4 py-8 bg-[#09101A] border-[#55E6A5] border-x-2 rounded-none w-full"
-                    />
-                    <textarea
-                      placeholder="Message"
-                      className="textarea textarea-lg mt-4 px-4 py-6 bg-[#09101A] border-[#55E6A5] border-x-2 rounded-none w-full"
-                      rows={5}
-                    ></textarea>
-                    <input
-                      type="submit"
-                      value="Submit Now"
-                      className="mt-4 px-6 py-4 cursor-pointer bg-[#55E6A5] text-black font-semibold text-lg hover:bg-[#141C27] hover:text-white transition-colors duration-500 border-none nav-animation"
-                    />
-                  </form>
-                </div>
+                <RxCross2
+                  className={`absolute text-black transition-opacity duration-300 ${
+                    isOpen ? "opacity-100" : "opacity-0"
+                  }`}
+                />
               </div>
-            </div>
+            </button>
           </div>
         </div>
       </div>
-    </div>
+      {/* mobile device nav */}
+      <div
+        className={`transform transition-all duration-500 ease-in-out uppercase nav-link ${
+          isOpen
+            ? "opacity-100 translate-y-0 scale-100 visible"
+            : "opacity-0 -translate-y-5 scale-95 invisible"
+        } lg:hidden text-white mt-16 py-5 space-y-4 border-y border-y-[#55E6A5] bg-[#09101A] text-center`}
+      >
+        <Link
+          onClick={() => window.scrollTo(0, 0)}
+          className="text-sm font-semibold nav-animation block w-32 mx-auto"
+          to="/"
+        >
+          Home
+        </Link>
+        <a
+          className="text-sm font-semibold nav-animation block w-32 mx-auto"
+          href="/#about"
+        >
+          About
+        </a>
+        <a
+          className="text-sm font-semibold nav-animation block w-32 mx-auto"
+          href="/#service"
+        >
+          Service
+        </a>
+        <a
+          className="text-sm font-semibold nav-animation block w-32 mx-auto"
+          href="/#project"
+        >
+          Project
+        </a>
+        <a
+          className="text-sm font-semibold nav-animation block w-32 mx-auto"
+          href="/#skills"
+        >
+          Skills
+        </a>
+        <Link
+          className="text-sm font-semibold nav-animation block w-32 mx-auto"
+          to="/blog"
+        >
+          Blog
+        </Link>
+        <Link
+          className="text-sm font-semibold nav-animation block w-32 mx-auto"
+          to="/contact"
+        >
+          Contact
+        </Link>
+      </div>
+    </>
   );
 };
 
